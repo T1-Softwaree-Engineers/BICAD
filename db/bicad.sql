75a9ba46f34694c7b2b90b589701923938b83930
Enrique Ochoa <73363057+Jk99z2@users.noreply.github.com>
2022-06-03 11:40:19 -0500

    Actualización: Base de datos
    Se actualiza la base de datos.

--- db/bicad.sql
+++ db/bicad.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2022 a las 16:14:21
--- Versión del servidor: 8.0.28
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 03-06-2022 a las 18:34:48
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bicad`
 --
+CREATE DATABASE IF NOT EXISTS `bicad` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bicad`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adultosm`
+--
+
+CREATE TABLE IF NOT EXISTS `adultosm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ecuidador` varchar(30) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `edad` int(2) NOT NULL,
+  `genero` varchar(25) NOT NULL,
+  `contacto` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ecuidador` (`ecuidador`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `adultosm`
+--
+
+INSERT INTO `adultosm` (`id`, `ecuidador`, `nombre`, `apellido`, `edad`, `genero`, `contacto`) VALUES
+(1, 'eochoa11@ucol.mx', 'Enrique de Jesús', 'Ochoa Preciado', 21, 'Hombre', '3141076334');
 
 -- --------------------------------------------------------
 
@@ -27,48 +54,37 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(40) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
-  `email` varchar(40) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `contacto` varchar(15) NOT NULL,
   `pwd` varchar(150) NOT NULL,
-  `role` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `role` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `contacto` (`contacto`),
+  KEY `email` (`email`),
+  KEY `email_2` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `username`, `nombre`, `apellidos`, `email`, `contacto`, `pwd`, `role`) VALUES
-(1, 'eochoa11', 'Enrique de Jesús', 'Ochoa Preciado', 'eochoa11@ucol.mx', '3141076334', 'e50d11e94bafcd7c78120d7a6478bce07c60b70207f7aa19e71ebdb37eecccbde475e051a28209fdfb5c9a217ea08d335422244574465e73c39863c8f18874fa', 'usuario'),
-(2, 'bicad1', 'Enrique de Jesús', 'Ochoa Preciado', 'enriciado@gmail.com', '3141109228', 'e50d11e94bafcd7c78120d7a6478bce07c60b70207f7aa19e71ebdb37eecccbde475e051a28209fdfb5c9a217ea08d335422244574465e73c39863c8f18874fa', 'usuario'),
-(3, 'bicad2', 'Edson', 'Manzano', 'pickerlife99@gmail.com', '3143575607', 'e50d11e94bafcd7c78120d7a6478bce07c60b70207f7aa19e71ebdb37eecccbde475e051a28209fdfb5c9a217ea08d335422244574465e73c39863c8f18874fa', 'usuario'),
-(4, 'Enrique', 'Enrique', 'Rosales', 'erosales@ucol.mx', '3141234567', '7b6ad79b346fb6951275343948e13c1b4ebca82a5452a6c5d15684377f096ca927506a23a847e6e046061399631b16fc2820c8b0e02d0ea87aa5a203a77c2a7e', 'usuario');
+(1, 'eochoa11', 'Enrique de Jesús', 'Ochoa Preciado', 'eochoa11@ucol.mx', '3141076334', 'e50d11e94bafcd7c78120d7a6478bce07c60b70207f7aa19e71ebdb37eecccbde475e051a28209fdfb5c9a217ea08d335422244574465e73c39863c8f18874fa', 'usuario');
 
 --
--- Índices para tablas volcadas
+-- Restricciones para tablas volcadas
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Filtros para la tabla `adultosm`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `email` (`email`),
-  ADD KEY `contacto` (`contacto`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `adultosm`
+  ADD CONSTRAINT `adultosm_ibfk_1` FOREIGN KEY (`ecuidador`) REFERENCES `usuarios` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
